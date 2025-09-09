b9e34e1cd47c00e67b61ef3058b72cf4af91bbbe
chomnr <hello@zeljko.me>
2025-09-09 16:46:56 -0400

    moved into /api instead of /api/auth

--- src/routes/api/*auth.ts
+++ src/routes/api/*auth.ts
@@ -1,4 +1,4 @@
-import { auth } from '../../../../auth'
+import { auth } from '../../../auth'
 import { toSolidStartHandler } from 'better-auth/solid-start'
 
 export const { GET, POST } = toSolidStartHandler(auth)
