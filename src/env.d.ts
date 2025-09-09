ee29d1b535319d6a6a9fe023381a4795f0914bc4
chomnr <hello@zeljko.me>
2025-09-09 18:36:03 -0400

    added more envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,5 +4,21 @@ declare namespace NodeJS {
     readonly DATABASE_URL: string
     readonly BETTER_AUTH_SECRET: string
     readonly BETTER_AUTH_URL: string
+
+    readonly AUTH_SECRET: string
+
+    readonly MAILER_AWS_ACCESS_KEY_ID: string
+    readonly MAILER_AWS_SECRET_ACCESS_KEY: string
+    readonly MAILER_AWS_REGION: string
+    readonly MAILER_AWS_SES_SOURCE: string
+
+    readonly STRIPE_SECRET_KEY: string
+    readonly STRIPE_WEBHOOK_SECRET: string
+
+    readonly GOOGLE_CLIENT_ID: string
+    readonly GOOGLE_CLIENT_SECRET: string
+
+    readonly MICROSOFT_CLIENT_ID: string
+    readonly MICROSOFT_CLIENT_SECRET: string
   }
 }
